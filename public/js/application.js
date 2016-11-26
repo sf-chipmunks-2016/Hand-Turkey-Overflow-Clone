b71463e7c3c751832eb1b1594dfff7eb318579d9
Katie O'Neill <katiemarieoneill@gmail.com>
2016-11-25 17:11:43 -0800

    Create votes controller, user show page, and ajax upvote button

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  
+  $('.vote-count').on('click', function(event) {
+    var voteField = $(this)
+    var voteableId = voteField.attr('voteable-id');
+    var voteableType = voteField.attr('voteable-type');
+    var upVote = voteField.attr('up-vote');
+
+    $.post('/votes', {
+      'voteable_type': voteableType,
+      'voteable_id': voteableId,
+      'up_vote': upVote,
+    }, function(data) {
+      if (data.success === true) {
+        voteField.text("Upvote " + data.vote_count)
+      }
+    });
+  });
 });
